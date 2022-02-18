e6c2f0258f6684cb1cf6d4d10f95e1f5ce7321ff
awaisspk <iamawaisiqball@gmail.com>
2022-02-18 18:16:54 +0500

    home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,40 @@
 import {NextUIProvider} from '@nextui-org/react';
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import Script from 'next/script';
 import type {AppProps} from 'next/app';
-import {darkTheme, lightTheme} from 'styles/next-theme';
+import {QueryClient, QueryClientProvider} from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools';
+import {darkTheme, globalStyles, lightTheme} from 'styles/next-theme';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 6,
+    },
+  },
+});
 function MyApp({Component, pageProps}: AppProps) {
+  globalStyles();
   return (
-    <NextThemesProvider
-      defaultTheme="system"
-      attribute="class"
-      value={{
-        light: lightTheme.className,
-        dark: darkTheme.className,
-      }}
-    >
-      <NextUIProvider>
-        <Component {...pageProps} />
-      </NextUIProvider>
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        defaultTheme="system"
+        attribute="class"
+        value={{
+          light: lightTheme.className,
+          dark: darkTheme.className,
+        }}
+      >
+        <NextUIProvider>
+          <Script
+            src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}`}
+            strategy="beforeInteractive"
+          ></Script>
+          <Component {...pageProps} />
+        </NextUIProvider>
+      </NextThemesProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,16 +3,55 @@ import {Header} from '@components/Header';
 import {List} from '@components/List';
 import {Map} from '@components/Map';
 import type {NextPage} from 'next';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {useMedia} from 'react-use';
+import {getPlaceDetails} from '@src/lib/fetch';
+import {Coords} from 'google-map-react';
+import {useQuery} from 'react-query';
+import {Bounds} from '@src/types';
 
 const Home: NextPage = () => {
   const isMd = useMedia('(max-width : 960px)', true);
-  const [isHideDetails, setIsHideDetail] = useState(false);
+
+  const [hideDetails, setHideDetails] = useState(false);
+  const [coordinates, setCoordinates] = useState<Coords>({
+    lat: 31.5204,
+    lng: 74.3587,
+  });
+
+  const [bounds, setBounds] = useState<Bounds | null>(null);
+  const [childClicked, setChildClicked] = useState(null);
+
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('0');
+  const {data, isError, isFetching} = useQuery(
+    [type, bounds?.ne.lat, bounds?.ne.lng],
+    () => getPlaceDetails({ne: bounds!.ne, sw: bounds!.sw, type}),
+    {
+      enabled: bounds !== null,
+      retry: true,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      staleTime: 6 * 1000 * 60,
+      placeholderData: [],
+      select: (data) =>
+        data.filter((place: any) => place.rating > rating && place.name !== ''),
+    }
+  );
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords}) =>
+      setCoordinates({lat: coords.latitude, lng: coords.longitude})
+    );
+  }, []);
 
   return (
     <Box>
-      <Header isMobile={isMd} hideDetails={setIsHideDetail} />
+      <Header
+        setCoordinates={setCoordinates}
+        isMobile={isMd}
+        hideDetails={setHideDetails}
+      />
       <Box
         css={{
           height: 'calc(100vh - 70px)',
@@ -49,8 +88,15 @@ const Home: NextPage = () => {
             isError={isError}
           />
         </Box>
+
         <Box>
-          <Map />
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={data}
+            handleChildClick={setChildClicked}
+          />
         </Box>
       </Box>
     </Box>
